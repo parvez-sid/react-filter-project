3d51a90992c92c3cdca511b184575051ffaf6bc4
Parvez Siddiqi <psiddique74@gmail.com>
2022-07-04 12:12:07 +0530

    filter and search

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,122 +1,126 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
 import About from "./About";
 import Products from "./Products";
 
-const Home = () => {
-    const dataItems = [
-        {
-          image: "sweets-1.jpeg",
-          title: "sweet item",
-          type: "sweets",
-          price: 5,
-        },
-        {
-          image: "cupcake-1.jpeg",
-          title: "cupcake item",
-          type: "cupcakes",
-          price: 5,
-        },
-        {
-          image: "cake-1.jpeg",
-          title: "cake item",
-          type: "cakes",
-          price: 5,
-        },
-        {
-          image: "doughnut-1.jpeg",
-          title: "dougnut item",
-          type: "doughnuts",
-          price: 5,
-        },
-        {
-          image: "sweets-2.jpeg",
-          title: "sweet item",
-          type: "sweets",
-          price: 10,
-        },
-        {
-          image: "cupcake-2.jpeg",
-          title: "cupcake item",
-          type: "cupcakes",
-          price: 10,
-        },
-        {
-          image: "cake-2.jpeg",
-          title: "cake item",
-          type: "cakes",
-          price: 10,
-        },
-        {
-          image: "doughnut-2.jpeg",
-          title: "dougnut item",
-          type: "doughnuts",
-          price: 10,
-        },
-        {
-          image: "sweets-3.jpeg",
-          title: "sweet item",
-          type: "sweets",
-          price: 15,
-        },
-        {
-          image: "cupcake-3.jpeg",
-          title: "cupcake item",
-          type: "cupcakes",
-          price: 15,
-        },
-        {
-          image: "cake-3.jpeg",
-          title: "cake item",
-          type: "cakes",
-          price: 15,
-        },
-        {
-          image: "doughnut-3.jpeg",
-          title: "doughnut item",
-          type: "doughnuts",
-          price: 15,
-        }
-    ];
+const dataItems = [
+  {
+    id: 1,
+    image: "sweets-1.jpeg",
+    title: "sweet item",
+    type: "sweets",
+    price: 5,
+  },
+  {
+    id: 2,
+    image: "cupcake-1.jpeg",
+    title: "cupcake item",
+    type: "cupcakes",
+    price: 5,
+  },
+  {
+    id: 3,
+    image: "cake-1.jpeg",
+    title: "cake item",
+    type: "cakes",
+    price: 5,
+  },
+  {
+    id: 4,
+    image: "doughnut-1.jpeg",
+    title: "dougnut item",
+    type: "doughnuts",
+    price: 5,
+  },
+  {
+    id: 5,
+    image: "sweets-2.jpeg",
+    title: "sweet item",
+    type: "sweets",
+    price: 10,
+  },
+  {
+    id: 6,
+    image: "cupcake-2.jpeg",
+    title: "cupcake item",
+    type: "cupcakes",
+    price: 10,
+  },
+  {
+    id: 7,
+    image: "cake-2.jpeg",
+    title: "cake item",
+    type: "cakes",
+    price: 10,
+  },
+  {
+    id: 8,
+    image: "doughnut-2.jpeg",
+    title: "dougnut item",
+    type: "doughnuts",
+    price: 10,
+  },
+  {
+    id: 9,
+    image: "sweets-3.jpeg",
+    title: "sweet item",
+    type: "sweets",
+    price: 15,
+  },
+  {
+    id: 10,
+    image: "cupcake-3.jpeg",
+    title: "cupcake item",
+    type: "cupcakes",
+    price: 15,
+  },
+  {
+    id: 11,
+    image: "cake-3.jpeg",
+    title: "cake item",
+    type: "cakes",
+    price: 15,
+  },
+  {
+    id: 12,
+    image: "doughnut-3.jpeg",
+    title: "doughnut item",
+    type: "doughnuts",
+    price: 15,
+  }
+];
 
-    // const filterItems = (event) => {
-    //     const filter = event.target.textContent;
-    //     const filteredItems = dataItems.filter((item) => item.type === filter);
-    //     renderHtml(filteredItems, false);
-    // };
-      
-    // const renderHtml = (data = [], renderInitial= false) => {
-    //     let renderedHtml = "";
-    //     const items = renderInitial ? dataItems :data ;
-    //     for (let item of items) {
-    //       renderedHtml += `<div className="col-10 col-sm-6 col-lg-4 mx-auto my-3 store-item ${item.type}" data-item="${item.type}">
-    //         <div className="card ">
-    //           <div className="img-container">
-    //             <img src="../assets/img/${item.image}" className="card-img-top store-img" alt="">
-    //             <span className="store-item-icon" onclick="addtoCart(${item.price})">
-    //               <i className="fas fa-shopping-cart"></i>
-    //             </span>
-    //           </div>
-    //           <div className="card-body">
-    //             <div className="card-text d-flex justify-content-between text-capitalize">
-    //               <h5 id="store-item-name">${item.title}</h5>
-    //               <h5 className="store-item-value">$ <strong id="store-item-price" className="font-weight-bold">${item.price}</strong></h5>
-    //             </div>
-    //           </div>
-    //         </div>
-    //       </div>`;
-    //     }
-    //     document.getElementById("store-items").innerHTML = renderedHtml;
-    // }
-    ;
+const Home = () => {
+  const [items, setItems] = useState([]);
+
+    useEffect(() => {
+      setItems(dataItems)
+    }, [])
+
+    const filterItems = (event) => {
+        const filter = event.target.dataset && event.target.dataset.filter;
+        const filteredItems = filter === "all" ? dataItems : dataItems.filter((item) => item.type === filter);
+        setItems(filteredItems)
+    };
+
+    const searchItems = () => {
+        const value = document.getElementById("search-item").value
+        value.toLowerCase().trim()
+
+        let filteredItems = dataItems.filter((item) => item.type.includes(value));
+        if(value === "") {
+          alert('Please write something to search...')
+        }
+        setItems(filteredItems)
+    }
+    
     return(
         <>
           <Header />
           <Banner />
           <About />
-          {/* <Products filterItems={filterItems} renderHtml={renderHtml} /> */}
-          <Products dataItems={dataItems} />
+          <Products dataItems={items} filterItems={filterItems} searchItems={searchItems} />
         </>
     )
 }
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,71 +1,66 @@
-import React, { useEffect } from "react";
+import React from "react";
 
-    const Products = ({dataItems}) => {
-    // const { filterItems, renderHtml } = props;
-
-    // useEffect(() => {
-    //     renderHtml(null, true)
-    // },[])
+    const Products = ({dataItems, filterItems, searchItems}) => {
 
     const renderItems = () => {
-        return (
-            dataItems.map((item) => {
-                return <div className="col-10 col-sm-6 col-lg-4 mx-auto my-3 store-item sweets" data-item="sweets">
-                    <div className="card ">
-                        <div className="img-container">
-                        <img src={require(`../assets/img/${item.image}`)} className="card-img-top store-img" alt="" />
-                        <span className="store-item-icon">
-                            <i className="fas fa-shopping-cart"></i>
-                        </span>
-                        </div>
-                        <div className="card-body">
-                        <div className="card-text d-flex justify-content-between text-capitalize">
-                            <h5 id="store-item-name">{item.title}</h5>
-                            <h5 className="store-item-value">$ <strong id="store-item-price" className="font-weight-bold">{item.price}</strong></h5>
-                        </div>
-                        </div>
-                    </div>
+      return (
+        dataItems.map((item) => {
+          return(
+            <div key={item.id} className="col-10 col-sm-6 col-lg-4 mx-auto my-3 store-item sweets" data-item="sweets">
+              <div className="card ">
+                  <div className="img-container">
+                  <img src={require(`../assets/img/${item.image}`)} className="card-img-top store-img" alt="" />
+                  <span className="store-item-icon">
+                      <i className="fas fa-shopping-cart"></i>
+                  </span>
+                  </div>
+                  <div className="card-body">
+                  <div className="card-text d-flex justify-content-between text-capitalize">
+                      <h5 id="store-item-name">{item.title}</h5>
+                      <h5 className="store-item-value">$ <strong id="store-item-price" className="font-weight-bold">{item.price}</strong></h5>
+                  </div>
+                  </div>
+              </div>
             </div>
-            })
-        )
+          )
+        })
+      )
     }
-    console.log(renderItems())
     return(
         <section id="store" className="store py-5">
-        <div className="container">
-          <div className="row">
-            <div className="col-10 mx-auto col-sm-6 text-center">
-              <h1 className="text-capitalize">our <strong className="banner-title ">store</strong></h1>
+          <div className="container">
+            <div className="row">
+              <div className="col-10 mx-auto col-sm-6 text-center">
+                <h1 className="text-capitalize">our <strong className="banner-title ">store</strong></h1>
+              </div>
             </div>
-          </div>
-          <div className="row">
-            <div className=" col-lg-8 mx-auto d-flex justify-content-around my-2 sortBtn flex-wrap">
-              <a href="#" className="btn btn-outline-secondary btn-black text-uppercase filter-btn m-2" data-filter="all"> all</a>
-              <a href="#" className="btn btn-outline-secondary btn-black text-uppercase filter-btn m-2" data-filter="cakes">cakes</a>
-              <a href="#" className="btn btn-outline-secondary btn-black text-uppercase filter-btn m-2" data-filter="cupcakes">cupcakes</a>
-              <a href="#" className="btn btn-outline-secondary btn-black text-uppercase filter-btn m-2" data-filter="sweets">sweets</a>
-              <a href="#" className="btn btn-outline-secondary btn-black text-uppercase filter-btn m-2" data-filter="doughnuts">doughnuts</a>
+            <div className="row">
+              <div className=" col-lg-8 mx-auto d-flex justify-content-around my-2 sortBtn flex-wrap">
+                <a href="#" className="btn btn-outline-secondary btn-black text-uppercase filter-btn m-2" data-filter="all" onClick={(e) => {filterItems(e)}}> all</a>
+                <a href="#" className="btn btn-outline-secondary btn-black text-uppercase filter-btn m-2" data-filter="cakes" onClick={(e) => {filterItems(e)}}>cakes</a>
+                <a href="#" className="btn btn-outline-secondary btn-black text-uppercase filter-btn m-2" data-filter="cupcakes" onClick={(e) => {filterItems(e)}}>cupcakes</a>
+                <a href="#" className="btn btn-outline-secondary btn-black text-uppercase filter-btn m-2" data-filter="sweets" onClick={(e) => {filterItems(e)}}>sweets</a>
+                <a href="#" className="btn btn-outline-secondary btn-black text-uppercase filter-btn m-2" data-filter="doughnuts" onClick={(e) => {filterItems(e)}}>doughnuts</a>
+              </div>
             </div>
-          </div>
-          <div className="row">
-    
-            <div className="col-10 mx-auto col-md-6">
-              <form>
-                <div className="input-group mb-3">
-                  <div className="input-group-prepend ">
-                    <span className="input-group-text search-box" id="search-icon"><i className="fas fa-search"></i></span>
+            <div className="row">
+      
+              <div className="col-10 mx-auto col-md-6">
+                <form>
+                  <div className="input-group mb-3">
+                    <div className="input-group-prepend" style={{cursor : "pointer"}} onClick={() => {searchItems()}} >
+                      <span className="input-group-text search-box" id="search-icon"><i className="fas fa-search"></i></span>
+                    </div>
+                    <input type="text" className="form-control" placeholder='item....' id="search-item" onKeyUp={(e) => {searchItems(e)}} />
                   </div>
-                  <input type="text" className="form-control" placeholder='item....' id="search-item" />
-                </div>
-    
-              </form>
+                </form>
+              </div>
+            </div>
+            <div className="row" id="store-items">
+              {renderItems()}
             </div>
           </div>
-          <div className="row" id="store-items">
-            {renderItems()}
-          </div>
-          </div>
-      </section>
+        </section>
     )
 }
 export default Products

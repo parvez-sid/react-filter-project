703b26c734230457f9b9e88b5c1af9708759e943
Parvez Siddiqi <psiddique74@gmail.com>
2022-07-04 12:11:49 +0530

    Link tag and scroll

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,31 @@
 import React from "react";
 import logo from '../assets/img/logo.svg'
+import { Link } from "react-router-dom";
 
 const Header = () => {
+
+    const scroll = (e, id) => {
+        e.preventDefault()
+        const element = document.getElementById(id);
+        element.scrollIntoView( { behavior: 'smooth', block: 'start' } );
+    };
+
     return(
         <nav className="navbar navbar-expand-lg px-4">
-        <a className="navbar-brand" href="#"><img src={logo} alt="" /></a>
+        <Link className="navbar-brand" to={"/"}><img src={logo} alt="" /></Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNav">
             <span className="toggler-icon"><i className="fas fa-bars"></i></span>
         </button>
         <div className="collapse navbar-collapse" id="myNav">
             <ul className="navbar-nav mx-auto text-capitalize">
             <li className="nav-item active">
-                <a className="nav-link" href="#">home</a>
+                <Link className="nav-link" to={"#"}>home</Link>
             </li>
             <li className="nav-item">
-                <a className="nav-link " href="#">about</a>
+                <Link className="nav-link " to={"#"} onClick={(e) => {scroll(e,"about")}}>about</Link>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="#">store</a>
+                <Link className="nav-link" to={"#"} onClick={(e) => {scroll(e, "store")}}>store</Link>
             </li>
             </ul>
             <div className="nav-info-items d-none d-lg-flex ">

ef84fa8ffb999b35741611dfbaaf26e761acdf57
Parvez Siddiqi <psiddique74@gmail.com>
2022-07-13 14:31:50 +0530

    try catch in actions

--- src/store/actions/products.js
+++ src/store/actions/products.js
@@ -3,34 +3,47 @@ import {sweetErrAlert} from '../../utilities/Utility';
 
 const setProducts = (data) => (dispatch) => {
 
-    dispatch({
-        type: SET_PRODUCTS,
-        payload: data
-    })
+    try {
+        dispatch({
+            type: SET_PRODUCTS,
+            payload: data
+        })
+    } catch (error) {
+        console.log(err);
+    }
 }
 
 const filterProducts = (items, filter) => (dispatch) => {
+    try{
+        const filteredItems = filter === "all" ? items : items.filter((item) => item.type === filter);
     
-    const filteredItems = filter === "all" ? items : items.filter((item) => item.type === filter);
+        dispatch({
+            type: FILTER_PRODUCTS,
+            payload : filteredItems
+        })
+    }
+    catch(err) {
+        console.log(err);
+    }
     
-    dispatch({
-        type: FILTER_PRODUCTS,
-        payload : filteredItems
-    })
 }
 
 const searchProducts = (items, value) => (dispatch) => {
-    if(value === "") {
-        sweetErrAlert('Please write something to search...', 'Ok')
+    try {
+        if(value === "") {
+            sweetErrAlert('Please write something to search...', 'Ok')
+        }
+    
+        const searchedItems = items.filter((item) => item.type.includes(value));
+        if(searchedItems.length === 0) {sweetErrAlert('No items to show with this keyword!', 'Ok')}
+        const action = {
+            type: SEARCH_PRODUCTS,
+            payload : searchedItems
+        }
+        dispatch(action)
+    } catch (error) {
+        console.log(err);
     }
-
-    const searchedItems = items.filter((item) => item.type.includes(value));
-    if(searchedItems.length === 0) {sweetErrAlert('No items to show with this keyword!', 'Ok')}
-    const action = {
-        type: SEARCH_PRODUCTS,
-        payload : searchedItems
-    }
-    dispatch(action)
 }
 
 export {
